48dc1857eb5ed82f9dafad6c1a2f17459565b18f
Johnny <johnny_s.o@hotmail.com>
2023-01-03 18:54:27 -0300

    Adicionando clãs e especializações

--- src/components/Ficha/Ficha.jsx
+++ src/components/Ficha/Ficha.jsx
@@ -1,28 +1,55 @@
 import React from 'react';
 import './Ficha.css';
 
-export default function Ficha(){
+export default function Ficha() {
 
   const clans = [
     'Aburame',
     'Akimichi',
     'Hozuki',
     'Hyuuga',
-    'Inuzuka', 
-    'Kaguya', 
-    'Koori', 
-    'Kugutsu', 
-    'Nara',   
+    'Inuzuka',
+    'Kaguya',
+    'Koori',
+    'Kugutsu',
+    'Nara',
+    'Senju',
+    'Uchiha',
+    'Yamanaka',
   ]
 
-  return(
+  const specs = [
+    'Genjutsu', 
+    'Ninjutsu', 
+    'Taijutsu', 
+    'Doton', 
+    'Fuuton', 
+    'Katon', 
+    'Raiton', 
+    'Suiton', 
+    'Fuuinjutsu',
+  ];
+
+  return (
     <div>
       <h1>Atualização de Ficha</h1>
-      <label>Clã:</label>
-      <select name="clans" id="clans" >
-        {clans.map(clans => (
-          <option key={clans} value={clans}>
-            {clans}
+
+      <div className="ficha__clans">
+        <label>Clã:</label>
+        <select name="clans" id="clans" >
+          {clans.map(clans => (
+            <option key={clans} value={clans}>
+              {clans}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <label>Especializações:</label>
+      <select name="specs" id="specs" >
+        {specs.map(specs => (
+          <option key={specs} value={specs}>
+            {specs}
           </option>
         ))}
       </select>
