813649028dfb956c9ec6ad581093d369e4cdd613
Johnny <johnny_s.o@hotmail.com>
2023-01-03 18:45:07 -0300

    Adicionado lista de clãs

--- src/components/Ficha/Ficha.jsx
+++ src/components/Ficha/Ficha.jsx
@@ -2,10 +2,30 @@ import React from 'react';
 import './Ficha.css';
 
 export default function Ficha(){
+
+  const clans = [
+    'Aburame',
+    'Akimichi',
+    'Hozuki',
+    'Hyuuga',
+    'Inuzuka', 
+    'Kaguya', 
+    'Koori', 
+    'Kugutsu', 
+    'Nara',   
+  ]
+
   return(
     <div>
       <h1>Atualização de Ficha</h1>
-      
+      <label>Clã:</label>
+      <select name="clans" id="clans" >
+        {clans.map(clans => (
+          <option key={clans} value={clans}>
+            {clans}
+          </option>
+        ))}
+      </select>
     </div>
   )
 }
