075ad2de45d01da1f0c729861bfa5e42224a3339
Johnny <johnny_s.o@hotmail.com>
2023-01-05 07:48:30 -0300

    Adicionada interação para que possa aumentar o atributo de acordo com os botões

--- src/components/Atributes/Atributes.jsx
+++ src/components/Atributes/Atributes.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import './Atributes.css'
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -7,28 +7,37 @@ import ListItemText from '@mui/material/ListItemText';
 import Atributes from '../Atributes/Atributes'
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-
 
 export default function AtributesChar({ atb }) {
+  const [points, setPoints] = useState(1);
+
+  const handleIncrement = () => {
+    setPoints(points + 1);
+  };
+
+  const handleDecrement = () => {
+    setPoints(points - 1);
+  };
+
   return (
     <>
-    <List className="list__atributes">
-      <ListItem>
-        <ListItemButton>
-          <ListItemText primary={atb} />
-          <ButtonGroup
-            disableElevation
-            variant="contained"
-            aria-label="Disabled elevation buttons"
-          >
-            <Button>+</Button>
-            <Button>-</Button>
-          </ButtonGroup>
-        </ListItemButton>
-      </ListItem>
-    </List>
-    
+      <List className="list__atributes">
+        <ListItem>
+          <ListItem>
+            <ListItemText primary={`${atb} - ${points}`} />
+            <ButtonGroup
+              disableElevation
+              variant="contained"
+              aria-label="Disabled elevation buttons"
+              size="small"
+            >
+              <Button onClick={handleIncrement}>+</Button>
+              <Button onClick={handleDecrement}>-</Button>
+            </ButtonGroup>
+          </ListItem>
+        </ListItem>
+      </List>
+
     </>
   )
 }
