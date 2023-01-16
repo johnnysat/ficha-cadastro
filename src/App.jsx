820134e083a4db28d5169ef8cc992c946806ed59
Johnny <johnny_s.o@hotmail.com>
2023-01-16 07:06:07 -0300

    Adicionando barra de navegação com specs principais

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Ficha from './components/Ficha/Ficha'
 import CharDate from './components/CharDate/CharDate';
+import Navigation from './components/Navigation/Navigation'
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <div className="body__app">
       <Ficha />
       <CharDate />
+      </div>
     </div>
   );
 }
